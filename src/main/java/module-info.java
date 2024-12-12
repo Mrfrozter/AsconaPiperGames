7d2a868e57b54faea32603cbbd64383332c9d9b4
AnnaB <altea200@hotmail.com>
2024-12-12 10:13:32 +0100

    match and MatchDAO added. helloController deleted HelloApplication modified --- needs more.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module hill.ascona.asconapipergames {
     requires javafx.controls;
+    requires javafx.fxml;
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
-    requires java.desktop;
 
-    opens hill.ascona.asconapipergames.entities to org.hibernate.orm.core;
+    opens hill.ascona.asconapipergames to javafx.fxml;
     exports hill.ascona.asconapipergames;
 }

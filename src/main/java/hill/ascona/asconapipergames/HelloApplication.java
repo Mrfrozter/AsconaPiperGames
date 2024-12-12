8515b5bbc44d1f5b47ba60ba3eb7aaba55c86753
AnnaB <altea200@hotmail.com>
2024-12-12 09:43:01 +0100

    match and MatchDAO added. helloController deleted HelloApplication modified --- needs more.

--- src/main/java/hill/ascona/asconapipergames/HelloApplication.java
+++ src/main/java/hill/ascona/asconapipergames/HelloApplication.java
@@ -1,23 +1,41 @@
 package hill.ascona.asconapipergames;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.example.asconapipergames.DAO.MatchDAO;
+import org.example.asconapipergames.entities.Match;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+
+        MatchDAO matchDAO = new MatchDAO();
+        Match testMatch = new Match("t7u9p");
+
+        if(matchDAO.saveMatch(testMatch)){
+            System.out.println("Match Saved");
+        } else {
+            System.out.println("Match Not Saved");
+        }
+
+
+
+
+        AnchorPane anchorPane = new AnchorPane();
+        Scene scene = new Scene(anchorPane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }

daf8e94d381c6755c4be6d189eb370478e60ae9b
Mrfrozter <lauriblomhovi@gmail.com>
2024-12-13 11:15:39 +0100

    Created Game 2

--- src/main/java/hill/ascona/asconapipergames/entities/Game.java
+++ src/main/java/hill/ascona/asconapipergames/entities/Game.java
@@ -3,40 +3,56 @@ package hill.ascona.asconapipergames.entities;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "games")
+@Table(name = "game")
 public class Game {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "game_id")
     private int id;
 
-    @Column(name = "game_title", length = 50, nullable = false)
+    @Column(name = "game_title", length = 100, nullable = false)
     private String title;
 
-    @Column(name = "game_numberOfTeams", length = 50, nullable = false)
-    private String title;
-
-    @Column(name = "game_genre", length = 10, nullable = false)
+    @Column(name = "game_genre", length = 50, nullable = false)
     private String genre;
 
-    public Game() {
-        this.genre =genre;
-        this.title = title;
-    }
+    @Column(name = "game_numberOfTeams", nullable = false)
+    private int numberOfTeams;
+
+    // Getters and Setters
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
-    public String getGenre{
-        return.genre;
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getNumberOfTeams() {
+        return numberOfTeams;
+    }
+
+    public void setNumberOfTeams(int numberOfTeams) {
+        if (numberOfTeams <= 0) {
+            throw new IllegalArgumentException("Number of teams must be greater than 0.");
+        }
+        this.numberOfTeams = numberOfTeams;
     }
 }
 

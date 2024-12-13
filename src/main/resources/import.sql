991d00b9fdd5824f88e4a4758faa3f6d031c1965
AnnaB <altea200@hotmail.com>
2024-12-13 12:20:29 +0100

    .

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+/*INSERT INTO games(game_name) VALUES ('Halo 3');
+INSERT INTO games(game_name) VALUES ('Counter Strike 2');
+INSERT INTO games(game_name) VALUES ('Fortnite');
+INSERT INTO games(game_name) VALUES ('Dota 2');
+INSERT INTO games(game_name) VALUES ('LoL');
+INSERT INTO games(game_name) VALUES ('');
+INSERT INTO games(game_name) VALUES ('Valorant');
+*/

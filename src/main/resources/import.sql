c6c9451639a01948a7b8aa31cfd092199fa667d8
Mrfrozter <lauriblomhovi@gmail.com>
2024-12-19 11:20:52 +0100

    TripleGamePush delete iml

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,11 @@
+INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('Chess', 'Strategy', 2);
 INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('Halo 3', 'FPS', 8);
 INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('Counter Strike 2', 'FPS', 5);
 INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('Fortnite', 'BR', 20);
+INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('Dragonball Sparking Zero', 'Fighting', 2);
+INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('FallGuys', 'Party', 4);
+INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('MobileLegends', 'MOBA', 5);
+INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('DragonsDogma 2', 'RPG', 1);
 INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('Dota 2', 'MOBA', 5);
 INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('LoL', 'MOBA', 5);
 INSERT INTO game(game_title, game_genre, game_numberOfTeams) VALUES ('Valorant', 'FPS', 5);

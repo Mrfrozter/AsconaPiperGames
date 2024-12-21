bfe58836360185cf084c41846c3cf4b644c974f7
wiha97 <wiha97@duck.com>
2024-12-21 23:43:47 +0100

    cleaned up helloapp

--- src/main/java/hill/ascona/asconapipergames/HelloApplication.java
+++ src/main/java/hill/ascona/asconapipergames/HelloApplication.java
@@ -1,23 +1,9 @@
 package hill.ascona.asconapipergames;
 
-import hill.ascona.asconapipergames.DAO.MatchDAO;
-import hill.ascona.asconapipergames.DAO.PersonDAO;
-import hill.ascona.asconapipergames.entities.Match;
-import hill.ascona.asconapipergames.entities.Person;
 import hill.ascona.asconapipergames.managers.ViewManager;
-import hill.ascona.asconapipergames.views.GameView;
-import hill.ascona.asconapipergames.views.MatchView;
-import hill.ascona.asconapipergames.views.PersonView;
-import hill.ascona.asconapipergames.views.TournamentView;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 

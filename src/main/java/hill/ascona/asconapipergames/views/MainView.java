9844bec8e6dc3ae41261c671efe58350a323d55f
wiha97 <wiha97@duck.com>
2024-12-21 13:01:48 +0100

    Fixed usrbox

--- src/main/java/hill/ascona/asconapipergames/views/MainView.java
+++ src/main/java/hill/ascona/asconapipergames/views/MainView.java
@@ -1,25 +1,33 @@
 package hill.ascona.asconapipergames.views;
 
 import hill.ascona.asconapipergames.managers.ViewManager;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class MainView {
     public VBox start(){
         VBox box = new VBox();
         TabPane tabPane = new TabPane();
-        tabPane.setPrefSize(700,600);
+        box.setMaxSize(700,600);
 
-        Label logBtn = new Label("Logout");
-        logBtn.setText(ViewManager.getLoggedInUser());
-        logBtn.setMaxHeight(20);
-        logBtn.setOnMouseClicked((e)->{
+        Label usrLabel = new Label("Logged in as "+ViewManager.getLoggedInUser());
+        usrLabel.setMaxHeight(20);
+
+        HBox usrBox = new HBox();
+        Button logBtn = new Button("Logout");
+        logBtn.setOnAction((e)->{
             ViewManager.loginView();
         });
-        logBtn.setAlignment(Pos.CENTER_RIGHT);
+        usrBox.setPadding(new Insets(2));
+        usrBox.setSpacing(5);
+        usrBox.getChildren().addAll(usrLabel,logBtn);
+        usrBox.setAlignment(Pos.BASELINE_RIGHT);
 
         Tab tab1 = new Tab("Users or Players", new PersonView().start());
         Tab tab2 = new Tab("Teams", new Label("Show teams"));
@@ -33,7 +41,7 @@ public class MainView {
         tabPane.getTabs().add(tab4);
         tabPane.getTabs().add(tab5);
 
-        box.getChildren().addAll(tabPane, logBtn);
+        box.getChildren().addAll(tabPane, usrBox);
 
         return box;
     }
